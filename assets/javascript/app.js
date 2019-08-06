b42fa4dc8f5b2b5e0deae565d7137c4125b209eb
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-08-06 19:31:40 -0400

    added marker to map

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,30 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7549, lng: -73.9840},
-    zoom: 8
+  var myLatLng = {lat: 40.782710, lng: -73.965310};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    // title: 'Hello World!'
   });
 }
-// 40.7549° N, 73.9840° W
+
+var latlng = new google.maps.LatLng(40.782710, -73.965310);
+
+function addmarker(latilongi) {
+    var marker = new google.maps.Marker({
+        position: latilongi,
+        title: 'new marker',
+        draggable: true,
+        map: map
+    });
+    map.setCenter(marker.getPosition())
+}
+
+$('#btnaddmarker').on('click', function() {
+    addmarker(latlng)
+})
