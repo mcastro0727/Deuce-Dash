9c7af6da287d5b614f7d9899f351868d1141ac4e
blake <summersb2@montclair.edu>
2019-08-06 19:05:19 -0400

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,42 @@
-var map;
-function initMap() {
-     var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 40.7549, lng: -73.9840 },
-        zoom: 8
-    });
-}
+// $(function () {
 
-// function init() {
-//     var mapOptions = {
-//         center: new google.maps.LatLng(40.7549, -73.9840), mapTypeId: google.maps.mapTypeId.ROADMAP,
-//         zoom: 8
-//     };
-//     var venueMap;
-//     venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
-// }
+//     function initMap() {
 
-// function loadscript() {
+//         var location = new google.maps.LatLng(40.7549, -73.9840);
 
-//     var script = document.createElement('script');
-//     script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCkioyz1epNmUDEt2m_AnGPVYsD89b-E3g&callback=initializeApp';
-
-//     document.body.appendChild(script);
-
-// }
-
-// window.onload = loadscript;
-// var pinLocation = new google.maps.LatLng(40.7536, -73.9832);
-
-// var startPosition = new google.maps.Marker({
-//     position: pinLocation,
-//     map: map,
-//     icon: "img/go.png"
-
+//         var mapCanvas = document.getElementById('map');
+//         var mapOptions = {
+//             center: location,
+//             zoom: 8,
+//             panControl: false,
+//             mapTypeId: google.maps.MapTypeId.ROADMAP
+//         }
+//         var map = new google.maps.Map(mapCanvas, mapOptions);
+//     }
+//     google.maps.venet.addDomListener(window, 'load', initMap);
 // });
-// 40.7549° N, 73.9840° W
-// 40.7536, -73.9832//
+
+
+
+
+
+
+function initMap() {
+    var mapOptions = {
+        center: {
+            lat: 40.7549, lng: -73.9840
+        },
+        zoom: 8,
+    };
+    var map;
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+};
+
+var pinLocation = new.google.maps.LatLng(40.782710,-73.965310);
+
+var startPosition = new.google.maps.Marker({
+    position: pinLocation,
+    map: map,
+    icon: "assets/images/marker.png"
+});
