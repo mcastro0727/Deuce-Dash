c698d9bdbc037cb1d0d337b7e66af9278a4882cf
Melissa Castro <melissa.anne.castro@gmail.com>
2019-08-08 19:19:10 -0400

    moved js script below google map script

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,53 @@
 var myMap
 
 function initMap() {
-    var myLatLng = { lat: 40.782710, lng: -73.965310 };
+  var myLatLng = {
+    lat: 40.782710,
+    lng: -73.965310
+  };
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: myLatLng
-    });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: myLatLng
+  });
 
-    myMap = map
+  myMap = map
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-    });
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+  });
 
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var coord1 = new google.maps.LatLng(40.716180,-73.997490)
+  var coord1 = new google.maps.LatLng(40.716180, -73.997490)
   addMarker(coord1)
-  var coord2 = new google.maps.LatLng(40.721330,-74.012340)
+  var coord2 = new google.maps.LatLng(40.721330, -74.012340)
   addMarker(coord2)
-  var coord3 = new google.maps.LatLng(40.716290,-73.983770)
+  var coord3 = new google.maps.LatLng(40.716290, -73.983770)
   addMarker(coord3)
-  var coord4 = new google.maps.LatLng(40.756770,-73.970790)
+  var coord4 = new google.maps.LatLng(40.756770, -73.970790)
   addMarker(coord4)
-  var coord5 = new google.maps.LatLng(40.764910,-73.985110)
+  var coord5 = new google.maps.LatLng(40.764910, -73.985110)
   addMarker(coord5)
 
-  function addMarker(coordinates){
-    var marker = new google.maps.Marker({ 
+
+  function addMarker(coordinates) {
+    var marker = new google.maps.Marker({
       position: coordinates,
-      map: myMap, 
-      title: this.LocationName 
-    }); 
+      map: myMap,
+      title: this.LocationName
+    });
   }
 
-  function convertLocation(location){
-  var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyCkioyz1epNmUDEt2m_AnGPVYsD89b-E3g"
+  function convertLocation(location) {
+    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyCkioyz1epNmUDEt2m_AnGPVYsD89b-E3g"
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
       console.log(response)
       var lat = response.results[0].geometry.location.lat
       var lng = response.results[0].geometry.location.lng
@@ -55,13 +59,13 @@ $(document).ready(function(){
   convertLocation(addPlus("263 Mountain Way, Rutherford, NJ"))
 
 
-  function addPlus(string){
+  function addPlus(string) {
     stringArray = string.split(" ")
     var stringPlus = stringArray[0]
-    for(i=1; i<stringArray.length; i++){
+    for (i = 1; i < stringArray.length; i++) {
       stringPlus = stringPlus + "+" + stringArray[i]
     }
-    return(stringPlus)
+    return (stringPlus)
   }
 
 
@@ -71,36 +75,28 @@ $(document).ready(function(){
 
   var span = document.getElementsByClassName("close")[0];
 
-  $("#add-button").on("click", function() {
-    $(".modal").css("display","block")
-    
-    $("#submit").on("click", function(){
+  $("#add-button").on("click", function () {
+    $(".modal").css("display", "block")
+
+    $("#submit").on("click", function () {
       convertLocation(addPlus($("#address-input").val()))
       $("#address-input").val("")
 
     })
-    $("#cancel").on("click", function(){
-      
+    $("#cancel").on("click", function () {
+
       $("#address-input").val("")
 
     })
   })
 
-  $(".button").on("click", function() {
+  $(".button").on("click", function () {
     modal.style.display = "none";
   })
 
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
   }
 })
-
-
-
-
-
-
-
-
