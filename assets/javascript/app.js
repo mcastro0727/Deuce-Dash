c572d4645bce1cce53b59d2d6a36148666adc950
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-08-08 21:29:10 -0400

    set up firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,104 +1,118 @@
+var config = {
+  apiKey: "AIzaSyAn5NoP9LgIzSdhe8-H_zmBhAUOxWz7Huc",
+  authDomain: "deuce-dash.firebaseapp.com",
+  databaseURL: "https://deuce-dash.firebaseio.com",
+  storageBucket: "deuce-dash.appspot.com"
+}
 
+firebase.initializeApp(config);
+
+var database = firebase.database();
 var myMap
 
 function initMap() {
-  var myLatLng = {lat: 40.782710, lng: -73.965310};
+  var myLatLng = {
+    lat: 40.782710,
+    lng: -73.965310
+  };
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: myLatLng
-  });
+  })
 
   myMap = map
 
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-  });
 
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  var coord1 = new google.maps.LatLng(40.716180,-73.997490)
-  addMarker(coord1)
-  var coord2 = new google.maps.LatLng(40.721330,-74.012340)
-  addMarker(coord2)
-  var coord3 = new google.maps.LatLng(40.716290,-73.983770)
-  addMarker(coord3)
-  var coord4 = new google.maps.LatLng(40.756770,-73.970790)
-  addMarker(coord4)
-  var coord5 = new google.maps.LatLng(40.764910,-73.985110)
-  addMarker(coord5)
+  database.ref().on("child_added", function (childSnapshot) {
+    var lat = childSnapshot.val().lat
+    var lng = childSnapshot.val().lng
+    var coord = new google.maps.LatLng(lat, lng)
+    addMarker(coord)
+  })
 
-  function addMarker(coordinates){
-    var marker = new google.maps.Marker({ 
+  function addMarker(coordinates) {
+    var marker = new google.maps.Marker({
       position: coordinates,
-      map: myMap, 
-      title: this.LocationName 
-    }); 
+      map: myMap,
+      title: "address"
+    })
+    marker.addListener('click', function () {
+      if (myMap.getZoom() == 12) {
+        myMap.setZoom(15)
+        myMap.setCenter(marker.getPosition())
+      }
+      else if(myMap.getZoom() == 15){
+        myMap.setZoom(18)
+        myMap.setCenter(marker.getPosition())
+      }
+      else {
+        myMap.setZoom(12)
+        myMap.setCenter(marker.getPosition())
+      }
+    })
   }
 
-  function convertLocation(location){
-  var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyCkioyz1epNmUDEt2m_AnGPVYsD89b-E3g"
+  function convertLocation(location) {
+    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyCkioyz1epNmUDEt2m_AnGPVYsD89b-E3g"
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response){
-      console.log(response)
+    }).then(function (response) {
+
       var lat = response.results[0].geometry.location.lat
       var lng = response.results[0].geometry.location.lng
       var coord = new google.maps.LatLng(lat, lng)
+
+      database.ref().push({
+        lat: lat,
+        lng: lng
+      })
+
       addMarker(coord)
-      // console.log(lng)
     })
   }
-  convertLocation(addPlus("263 Mountain Way, Rutherford, NJ"))
 
-  function addPlus(string){
+  function addPlus(string) {
     stringArray = string.split(" ")
     var stringPlus = stringArray[0]
-    for(i=1; i<stringArray.length; i++){
+    for (i = 1; i < stringArray.length; i++) {
       stringPlus = stringPlus + "+" + stringArray[i]
     }
-    return(stringPlus)
+    return (stringPlus)
   }
 
-
   var modal = document.getElementById("modal");
-
   var btn = document.getElementById("myBtn");
-
   var span = document.getElementsByClassName("close")[0];
 
-  $("#add-button").on("click", function() {
-    $(".modal").css("display","block")
-    
-    $("#submit").on("click", function(){
+  $("#add-button").on("click", function () {
+    $(".modal").css("display", "block")
+
+    $("#submit").on("click", function () {
       convertLocation(addPlus($("#address-input").val()))
       $("#address-input").val("")
-
     })
-    $("#cancel").on("click", function(){
-      
-      $("#address-input").val("")
 
+    $("#cancel").on("click", function () {
+      $("#address-input").val("")
     })
   })
 
-  $(".button").on("click", function() {
+  $(".button").on("click", function () {
     modal.style.display = "none";
   })
 
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
   }
+
+
+
 })
-
-
-
-
-
-
