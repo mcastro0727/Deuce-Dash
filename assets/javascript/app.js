46eba6bdcffbf8a26afbeff9e6050b03c79a17de
blake <summersb2@montclair.edu>
2019-08-08 21:17:22 -0400

    todays push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,13 @@
+var config = {
+  apiKey: "AIzaSyAn5NoP9LgIzSdhe8-H_zmBhAUOxWz7Huc",
+  authDomain: "deuce-dash.firebaseapp.com",
+  databaseURL: "https://deuce-dash.firebaseio.com",
+  storageBucket: "deuce-dash.appspot.com"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
 var myMap
 
 function initMap() {
